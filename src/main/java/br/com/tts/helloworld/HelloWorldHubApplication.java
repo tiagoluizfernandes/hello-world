35d88f03adf659b743597ef365241c3eb8990e12
Tiago Luiz Fernandes <tiagoluiz@gft.com>
2023-03-06 08:24:12 -0300

    review

--- src/main/java/br/com/tts/helloworld/HelloWorldHubApplication.java
+++ src/main/java/br/com/tts/helloworld/HelloWorldHubApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldHubApplication {
 
 	public static void main(String[] args) {
+
+
+		//System.out.println(" ".isBlank()); //true
 		SpringApplication.run(HelloWorldHubApplication.class, args);
+
+
+
 	}
 
 }
